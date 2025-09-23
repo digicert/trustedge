5c90109d727d1eb0574f4d66c39456d1202fc37b
Shrey Tandel <standel@mocana.com>
2025-09-23 16:06:05 -0700

    Update variable names

--- examples/pqc-demo/est_server_keygen_mldsa.sh
+++ examples/pqc-demo/est_server_keygen_mldsa.sh
@@ -2,20 +2,20 @@
 
 set -e
 
-EST_SERVER_DN=""
-EST_SERVER_URL=""
-EST_USER=""
-EST_PASSWORD=""
-EST_CRED_ARG=""
+ESTC_SERVER_DN=""
+ESTC_SERVER_URL=""
+ESTC_USER=""
+ESTC_PASSWORD=""
+ESTC_CRED_ARG=""
 
 function help() {
-    echo "Usage: $0 --est-server-dn <server name> --est-server-url <url> [--est-user <username>] [--est-password <password>]"
+    echo "Usage: $0 --estc-server-dn <server name> --estc-server-url <url> [--estc-user <username>] [--estc-password <password>]"
     echo ""
     echo "Options:"
-    echo "  --est-server-dn       EST server name (e.g., demo.one.digicert.com)"
-    echo "  --est-server-url      URL of the EST server (e.g., /.well-known/est/devicetrustmanager/IOT_9a3d5e22-17c4-4f56-88a7-7cf8c914fa61)"
-    echo "  --est-user            [Optional] Username for EST authentication"
-    echo "  --est-password        [Optional] Password for EST authentication"
+    echo "  --estc-server-dn      EST server name (e.g., demo.one.digicert.com)"
+    echo "  --estc-server-url     URL of the EST server (e.g., /.well-known/est/devicetrustmanager/IOT_9a3d5e22-17c4-4f56-88a7-7cf8c914fa61)"
+    echo "  --estc-user           [Optional] Username for EST authentication"
+    echo "  --estc-password       [Optional] Password for EST authentication"
     echo "  --help                Display this help message"
     echo ""
 }
@@ -28,7 +28,7 @@ while [[ "$#" -gt 0 ]]; do
                 echo "Error: --estc-server-dn requires a value.";
                 exit 1;
             fi
-            EST_SERVER_DN="$2";
+            ESTC_SERVER_DN="$2";
             shift
             ;;
         --estc-server-url)
@@ -36,7 +36,7 @@ while [[ "$#" -gt 0 ]]; do
                 echo "Error: --estc-server-url requires a value.";
                 exit 1;
             fi
-            EST_SERVER_URL="$2";
+            ESTC_SERVER_URL="$2";
             shift
             ;;
         --estc-user)
@@ -44,7 +44,7 @@ while [[ "$#" -gt 0 ]]; do
                 echo "Error: --estc-user requires a value.";
                 exit 1;
             fi
-            EST_USER="$2";
+            ESTC_USER="$2";
             shift
             ;;
         --estc-password)
@@ -52,7 +52,7 @@ while [[ "$#" -gt 0 ]]; do
                 echo "Error: --estc-password requires a value.";
                 exit 1;
             fi
-            EST_PASSWORD="$2";
+            ESTC_PASSWORD="$2";
             shift
             ;;
         *)
@@ -64,18 +64,18 @@ while [[ "$#" -gt 0 ]]; do
     shift
 done
 
-if [ -z "$EST_SERVER_DN" ] || [ -z "$EST_SERVER_URL" ]; then
+if [ -z "$ESTC_SERVER_DN" ] || [ -z "$ESTC_SERVER_URL" ]; then
     help
     echo "Error: --estc-server-dn and --estc-server-url are required.";
     exit 1
 fi
 
-if [ -n "$EST_USER" ]; then
-    EST_CRED_ARG="--estc-user $EST_USER"
+if [ -n "$ESTC_USER" ]; then
+    ESTC_CRED_ARG="--estc-user $ESTC_USER"
 fi
 
-if [ -n "$EST_PASSWORD" ]; then
-    EST_CRED_ARG+=" --estc-pass $EST_PASSWORD"
+if [ -n "$ESTC_PASSWORD" ]; then
+    ESTC_CRED_ARG+=" --estc-pass $ESTC_PASSWORD"
 fi
 
 # Clean up any existing keys/certs
@@ -87,17 +87,17 @@ wget -P /etc/digicert/keystore/ca http://cacerts.digicert.com/DigiCertGlobalRoot
 
 # Get CA certs
 trustedge certificate est \
-    --estc-server-dn $EST_SERVER_DN \
-    --estc-server-url $EST_SERVER_URL/cacerts
+    --estc-server-dn $ESTC_SERVER_DN \
+    --estc-server-url $ESTC_SERVER_URL/cacerts
 
 # Issue certificate using server keygen
 trustedge certificate est \
-    --estc-server-dn $EST_SERVER_DN \
-    --estc-server-url $EST_SERVER_URL/serverkeygen \
+    --estc-server-dn $ESTC_SERVER_DN \
+    --estc-server-url $ESTC_SERVER_URL/serverkeygen \
     --estc-authentication-mode BASIC \
     --algorithm QS \
     --pq-alg MLDSA_44 \
     --key-alias mldsa_server_keygen \
     --csr-conf server_csr.cnf \
     --log-level INFO \
-    $EST_CRED_ARG
+    $ESTC_CRED_ARG
